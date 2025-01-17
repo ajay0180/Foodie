d728abda7c14345d45452e5235a3672597962f30
ajay0180 <jangraajay489@gmail.com>
2025-01-17 23:41:48 +0530

    final phase 1.1

--- server/index.js
+++ server/index.js
@@ -7,9 +7,15 @@ import FoodRoutes from "./routes/Food.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true })); // for form data
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/user/", UserRoutes);
 app.use("/api/food/", FoodRoutes);
