f21f41f15c733c9bcb0138689e31c22e02b94369
ajay0180 <jangraajay489@gmail.com>
2025-01-18 00:35:12 +0530

    final phase 2.2

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: ["https://foodie-frontend-psi.vercel.app", "http://localhost:3000"],
+    origin: ["*"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
